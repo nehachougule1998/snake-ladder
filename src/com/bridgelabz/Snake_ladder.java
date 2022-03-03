20436e2c055140bb5ba43a57d33b9b57f5838636
nehachougule1998 <nchougule734@gmail.com>
2022-03-03 17:48:45 +0530

    game start with 0

--- src/com/bridgelabz/Snake_ladder.java
+++ src/com/bridgelabz/Snake_ladder.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 import java.util.Random;
 public class Snake_ladder {
-     
+    int startposition = 0;     
 	public static void main(String[] args) {
 	 System.out.println("welcome to snake and ladder game");
 
